df77c8da4067ee991ec6d2280b509f74331a50fc
Alex Meyer <alexmeyer27@gmail.com>
2018-09-03 11:18:26 -0600

    building out dom functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+//Default page here
 
-
+//main API logic
 var ajaxCall = function(){
+	//jQuery ajax call
 	$.ajax({
   		method: "GET",
   		crossDomain: true,
@@ -11,7 +13,10 @@ var ajaxCall = function(){
  	 		country: 'us',
   			language: 'en'
  	 	},
+ 	 	//most of the logic lives within the success function
   		success: function(result) {
+  			var i = 0;
+  			var ajaxResult = result;
   			var articleObjectCreator = function (source, author, title, description, url, urlToImage, publishedAt) {
 				this.source = source;
 				this.author = author;
@@ -21,15 +26,15 @@ var ajaxCall = function(){
 				this.urlToImage = urlToImage;
 				this.publishedAt = publishedAt;
 			}
-  			var i = 0;
-  			var ajaxResult = result;
  			//go into array and make new object with each article
 			for (i = 0; i < ajaxResult.articles.length; i++){
 				var articles = ajaxResult.articles;
 				var article = new articleObjectCreator(articles[i].source, articles[i].author, articles[i].title, articles[i].description, articles[i].url, articles[i].urlToImage, articles[i].publishedAt)
-				$(".featuredImage").attr("src", "article.urlToImage");
+				//append each article to a component
+				createComponent();
+				appendToPage();
 				console.log(article);
-				return article;
+				// return article;
 			};
   			console.log(ajaxResult);
  	 	},
@@ -38,31 +43,39 @@ var ajaxCall = function(){
   		}
 	})};
 
+//On load it should make the intial api call
 (function () {
+	//When the application first loads display the loading container (see below on instructions to toggle this). When you successfully retrieve information from the default API hide the loader and replace the content of the `#main` container with that of the API. The DOM structure of each article must adhere to the `.article` structure.
+	$( "#popUp" ).toggleClass( "hidden", false );
     ajaxCall();
-    var appendToPage = function(){
-    	
-	}
+    $(document).ajaxSuccess(function(){
+    	$( "#popUp" ).toggleClass( "hidden", true );
+    })
 })();
 
-$( window ).on( "load", function() { 
-
-})
-
-
-/*
-  Please add all Javascript code to this file.
-*/
-
-//Default page here
-
-//On load it should make the intial api call
-
-
-//When the application first loads display the loading container (see below on instructions to toggle this). When you successfully retrieve information from the default API hide the loader and replace the content of the `#main` container with that of the API. The DOM structure of each article must adhere to the `.article` structure.
-
-
-//When the app is first loading and when the user selects to load a new feed from the dropdown, display the `#popUp` container with the `.loader` class. You can toggle the `.hidden` class from the container to display/hide the overlay container.
-
-
 //Clicking/tapping the "Feedr" logo will display the main/default feed.
+$("#defaultFeed").click(function(){
+	event.preventDefault()
+	ajaxCall()
+	}
+);
+
+var createComponent = function () {
+	//Each article must provide an image source for the circular thumbnail at the left of the article.
+	//Must provide a point, ranking, or some type of total impressions for the respective article.
+	//Must provide either a category, tag, or custom taxonomy to display below the title (of course title of article is also required).
+	//Must provide either a full version or a summary of the article for the pop up screen.
+	//When the user selects an article's title show the `#popUp` overlay. The content of the article must be inserted in the `.container` class inside `#popUp`. Make sure you remove the `.loader` class when toggling the article information in the pop-up.
+	//Change the link of the "Read more from source" button to that of the respective article.
+}
+
+var appendToPage = function () {
+
+}
+
+
+//Add functionality to hide the pop-up when user selects the "X" button on the pop-up.
+	//When the app is first loading and when the user selects to load a new feed from the dropdown, display the `#popUp` container with the `.loader` class. You can toggle the `.hidden` class from the container to display/hide the overlay container.
+
+
+
