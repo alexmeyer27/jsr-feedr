ec6b563cf7afbb0d975f3596fb9e44e1c9d2c35e
Alex Meyer <alexmeyer27@gmail.com>
2018-09-06 17:47:50 -0600

    final code commit

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
 //Default page here
-
-//On load it should make the intial api call
-
-
 let	source,
 	author,
 	title,
@@ -11,23 +7,42 @@ let	source,
 	urlToImage,
 	publishedAt;
 
+var popUpCreator = function(){
+			event.preventDefault()
+			$( "#popUp" ).removeClass()
+			
+			var selectedPopUpTitle = $(event.target).parent();
+			
+			var hiddenTitle = selectedPopUpTitle.find("#hiddenTitle").text();
+			$('#popUpTitle').text(hiddenTitle);
+
+			var hiddenDescription = selectedPopUpTitle.find("#hiddenDescription").text();
+			$("#description").text(hiddenDescription);
+
+			var hiddenURL = selectedPopUpTitle.find("#hiddenURL").text();
+			$(".popUpAction").attr("href", hiddenURL);
+	}				
+
+
+//On load it should make the intial api call
+
 //main API logic
-var ajaxCall = function(){
+var generalHeadlineCall = function(){
 	//jQuery ajax call
 	$.ajax({
- 		method: "GET",
- 		crossDomain: true,
- 		url: 'https://newsapi.org/v2/top-headlines',
- 		data: {
-  	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
-  	 		category: 'general',
+		method: "GET",
+		crossDomain: true,
+		url: 'https://newsapi.org/v2/top-headlines',
+		data: {
+ 	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
+ 	 		category: 'general',
 	 		country: 'us',
- 			language: 'en'
+			language: 'en'
 	 	},
 	 	//most of the logic lives within the success function
- 		success: function(result) {
- 			var i;
- 			var ajaxResult = result;
+		success: function(result) {
+			var i;
+			var ajaxResult = result;
 
 			//go into array and make new object with each article
 			for (i = 0; i < ajaxResult.articles.length; i++){
@@ -43,13 +58,19 @@ var ajaxCall = function(){
 					publishedAt = article.publishedAt;
 				
 				//defining article template
-				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><h3 id="articleTitle" class="popUpCreator">Test article title</h3></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
+				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><div id="hiddenWrapper" onclick="popUpCreator()"><h3 id="articleTitle" class="popUpCreator">Test article title</h3><p id="hiddenDescription" class="hidden"></p><p id="hiddenTitle" class="hidden"></p><p id="hiddenURL" class="hidden"></p></div></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
 				
 				//appending each article after the header
-				$("header").after(articleTemplate);
+				$("#popUp").after(articleTemplate);
 				
 				//adding h1/h3 article title
 				$("#articleTitle").html(title);
+				$("#hiddenTitle").html(title);
+				console.log(title);
+
+				$("#hiddenDescription").html(description);
+
+				$("#hiddenURL").html(url);
 				
 				//changing image to match article image
 				$('img[src=""]').attr("src", urlToImage);
@@ -57,64 +78,35 @@ var ajaxCall = function(){
 				// Must provide either a category, tag, or custom taxonomy to display below the title (of course title of article is also required).
 				$("#category").html("<h6>General</h6>");
 				
+				console.log(article);
 				}
 			i++
- 			console.log(ajaxResult);
- 			$( "#popUp" ).toggleClass( "hidden", true );
+			$( "#popUp" ).toggleClass( "hidden", true );
+			
 	 	},
- 		error: function() {
- 			alert("Error - api call unsuccessful. Check console");
- 		}
+		error: function() {
+			alert("Error - api call unsuccessful. Check console");
+		}
 	})};
-
-document.onload = ajaxCall();
-
-let titles = document.querySelectorAll("h3");
-
-// if (document.getElementById("protector").contains("h3")) 
-$(document).ajaxSuccess(function(){
-	titles.addEventListener("click", function(){
-		document.getElementById("popUp").classList.remove();
-	})
-})
-
-
-// var popUpCreator = document.getElementsByClassName("popUpCreator");
-
-				// var revealPopUp = function(){
-				// 	popUp.removeClass();
-				// 	$(".container").removeClass("hidden")
-				// 	$("#description").html("<p>" + description + "</p>")
-
-				// 	}
-
-				// popUpCreator.addEventListener("click", revealPopUp);
-
 
 
 (function () {
 	//When the application first loads display the loading container (see below on instructions to toggle this). When you successfully retrieve information from the default API hide the loader and replace the content of the `#main` container with that of the API. The DOM structure of each article must adhere to the `.article` structure.
-	$( "#popUp" ).toggleClass( "hidden", false );
+	$( "#popUp" ).toggleClass( "hidden", false )
+	generalHeadlineCall();
 })();
 
 
 //When the user selects an article's title show the `#popUp` overlay. The content of the article must be inserted in the `.container` class inside `#popUp`. Make sure you remove the `.loader` class when toggling the article information in the pop-up.
 
-// $("h3").each(function(){
-// 	$("h3").click(function(){
-// 		$("popUp").classList("");
-// 	})
-// })
 
-
-
+// document.onload = popUpCreator();
 
 
 //Clicking/tapping the "Feedr" logo will display the main/default feed.
 $("#defaultFeed").click(function(){
 	event.preventDefault()
 	ajaxCall()
-
 	}
 );	
 
@@ -138,24 +130,35 @@ searchImage.addEventListener('click', function(){
 
 //politico api call
 $("#politico").click(function(){
+	//attempt to clear out feed - not working
+	function clearOutFeed(){
+		var i;
+		var allElements = document.getElementsByClassName("data");
+		for ( i = 0 ; i < allElements.length; i++) {
+			allElements[i].innerHTML = "";
+		}
+	}
+
+	clearOutFeed();
+
 	//jQuery ajax call
 	$.ajax({
-  		method: "GET",
-  		crossDomain: true,
-  		url: 'https://newsapi.org/v2/top-headlines',
-  		data: {
-   	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
-   	 		sources: 'politico',
-  			language: 'en'
- 	 	},
+ 		method: "GET",
+ 		crossDomain: true,
+ 		url: 'https://newsapi.org/v2/top-headlines',
+ 		data: {
+  	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
+  	 		sources: 'politico',
+ 			language: 'en'
+	 	},
 //most of the logic lives within the success function
- 		success: function(result) {
- 			var i;
- 			var ajaxResult = result;
+		success: function(result) {
+			var i;
+			var ajaxResult = result;
 
 			//go into array and make new object with each article
 			for (i = 0; i < ajaxResult.articles.length; i++){
-				
+
 				var article = ajaxResult.articles[i];
 
 				let	source = article.source,
@@ -165,49 +168,59 @@ $("#politico").click(function(){
 					url = article.url,
 					urlToImage = article.urlToImage,
 					publishedAt = article.publishedAt;
-
+				
 				//defining article template
-				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><h3 id="articleTitle">Test article title</h3></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
+				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><div id="hiddenWrapper" onclick="popUpCreator()"><h3 id="articleTitle" class="popUpCreator">Test article title</h3><p id="hiddenDescription" class="hidden"></p><p id="hiddenTitle" class="hidden"></p><p id="hiddenURL" class="hidden"></p></div></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
 				
 				//appending each article after the header
-				$("header").after(articleTemplate);
+				$("#popUp").after(articleTemplate);
 				
 				//adding h1/h3 article title
 				$("#articleTitle").html(title);
+				$("#hiddenTitle").html(title);
+				console.log(title);
+
+				$("#hiddenDescription").html(description);
+
+				$("#hiddenURL").html(url);
 				
 				//changing image to match article image
 				$('img[src=""]').attr("src", urlToImage);
 				
 				// Must provide either a category, tag, or custom taxonomy to display below the title (of course title of article is also required).
 				$("#category").html("<h6>Politico</h6>");
+				
+				console.log(article);
 				}
 			i++
- 			console.log(ajaxResult);
+			$( "#popUp" ).toggleClass( "hidden", true );
+			
 	 	},
- 		error: function() {
- 			alert("Error - api call unsuccessful. Check console");
- 		}
+		error: function() {
+			alert("Error - api call unsuccessful. Check console");
+		}
 	})});
 
 //washiongton post api call
 $("#washingtonPost").click(function(){
 	$.ajax({
-  		method: "GET",
-  		crossDomain: true,
-  		url: 'https://newsapi.org/v2/top-headlines',
-  		data: {
-   	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
-   	 		sources: 'the-washington-post',
-  			language: 'en'
- 	 	},
- 	 	//most of the logic lives within the success function
- 		success: function(result) {
- 			var i;
- 			var ajaxResult = result;
+ 		method: "GET",
+ 		crossDomain: true,
+ 		url: 'https://newsapi.org/v2/top-headlines',
+ 		data: {
+  	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
+  	 		sources: 'the-washington-post',
+ 			language: 'en'
+	 	},
+	 	//most of the logic lives within the success function
+		success: function(result) {
+			var i;
+			var ajaxResult = result;
 
 			//go into array and make new object with each article
+			//go into array and make new object with each article
 			for (i = 0; i < ajaxResult.articles.length; i++){
-				
+
 				var article = ajaxResult.articles[i];
 
 				let	source = article.source,
@@ -217,51 +230,60 @@ $("#washingtonPost").click(function(){
 					url = article.url,
 					urlToImage = article.urlToImage,
 					publishedAt = article.publishedAt;
-
+				
 				//defining article template
-				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><h3 id="articleTitle">Test article title</h3></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
+				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><div id="hiddenWrapper" onclick="popUpCreator()"><h3 id="articleTitle" class="popUpCreator">Test article title</h3><p id="hiddenDescription" class="hidden"></p><p id="hiddenTitle" class="hidden"></p><p id="hiddenURL" class="hidden"></p></div></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
 				
 				//appending each article after the header
-				$("header").after(articleTemplate);
+				$("#popUp").after(articleTemplate);
 				
 				//adding h1/h3 article title
 				$("#articleTitle").html(title);
+				$("#hiddenTitle").html(title);
+				console.log(title);
+
+				$("#hiddenDescription").html(description);
+
+				$("#hiddenURL").html(url);
 				
 				//changing image to match article image
 				$('img[src=""]').attr("src", urlToImage);
 				
 				// Must provide either a category, tag, or custom taxonomy to display below the title (of course title of article is also required).
 				$("#category").html("<h6>Washington Post</h6>");
+				
+				console.log(article);
 				}
 			i++
- 			console.log(ajaxResult);
+			$( "#popUp" ).toggleClass( "hidden", true );
+			
 	 	},
- 		error: function() {
- 			alert("Error - api call unsuccessful. Check console");
- 		}
+		error: function() {
+			alert("Error - api call unsuccessful. Check console");
+		}
 	})});
 
 //new york times api call
 $("#newYorkTimes").click(function(){
 	//jQuery ajax call
 	$.ajax({
-  		method: "GET",
-  		crossDomain: true,
-  		url: 'https://newsapi.org/v2/top-headlines',
-  		data: {
-   	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
-   	 		sources: 'the-new-york-times',
-  			language: 'en'
- 	 	},
- 	 	//most of the logic lives within the success function
-  		//most of the logic lives within the success function
- 		success: function(result) {
- 			var i;
- 			var ajaxResult = result;
+ 		method: "GET",
+ 		crossDomain: true,
+ 		url: 'https://newsapi.org/v2/top-headlines',
+ 		data: {
+  	 		apiKey: '8535df6ab9464e74a2a8838bd8adec45',
+  	 		sources: 'the-new-york-times',
+ 			language: 'en'
+	 	},
+	 	//most of the logic lives within the success function
+ 		//most of the logic lives within the success function
+		success: function(result) {
+			var i;
+			var ajaxResult = result;
 
 			//go into array and make new object with each article
 			for (i = 0; i < ajaxResult.articles.length; i++){
-				
+
 				var article = ajaxResult.articles[i];
 
 				let	source = article.source,
@@ -271,26 +293,35 @@ $("#newYorkTimes").click(function(){
 					url = article.url,
 					urlToImage = article.urlToImage,
 					publishedAt = article.publishedAt;
-
+				
 				//defining article template
-				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><h3 id="articleTitle">Test article title</h3></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
+				let articleTemplate = '<section id="main" class="container"><article class="article"><section class="featuredImage"><img src="" alt="" /></section><section class="articleContent"><a href="#"><div id="hiddenWrapper" onclick="popUpCreator()"><h3 id="articleTitle" class="popUpCreator">Test article title</h3><p id="hiddenDescription" class="hidden"></p><p id="hiddenTitle" class="hidden"></p><p id="hiddenURL" class="hidden"></p></div></a><h6 id="category">Lifestyle</h6></section><div class="clearfix"></div></article>'
 				
 				//appending each article after the header
-				$("header").after(articleTemplate);
+				$("#popUp").after(articleTemplate);
 				
 				//adding h1/h3 article title
 				$("#articleTitle").html(title);
+				$("#hiddenTitle").html(title);
+				console.log(title);
+
+				$("#hiddenDescription").html(description);
+
+				$("#hiddenURL").html(url);
 				
 				//changing image to match article image
 				$('img[src=""]').attr("src", urlToImage);
 				
 				// Must provide either a category, tag, or custom taxonomy to display below the title (of course title of article is also required).
 				$("#category").html("<h6>New York Times</h6>");
+				
+				console.log(article);
 				}
 			i++
- 			console.log(ajaxResult);
+			$( "#popUp" ).toggleClass( "hidden", true );
+			
 	 	},
- 		error: function() {
- 			alert("Error - api call unsuccessful. Check console");
- 		}
+		error: function() {
+			alert("Error - api call unsuccessful. Check console");
+		}
 	})});
